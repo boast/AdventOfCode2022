d23fb8a07dc0ca98b2ecab1e4dd2fa5031ff0dd8
Dave Thalmann <thalmann@sogedes.com>
2022-12-01 13:07:05 +0100

    Prepare Day02

---
+++ src/aoc/days/Day02.java
@@ -0,0 +1,20 @@
+package aoc.days;
+
+import aoc.common.Day;
+
+import java.util.List;
+
+/**
+ * Day 2:
+ */
+public final class Day02 implements Day {
+    @Override
+    public String part1(final List<String> input) {
+        return "Not implemented Part 1"; //NON-NLS
+    }
+    
+    @Override
+    public String part2(final List<String> input) {
+        return "Not implemented Part 2"; //NON-NLS
+    }
+}
---
+++ test/aoc/days/Day02Test.java
@@ -0,0 +1,23 @@
+package aoc.days;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Day02Test extends AbstractDayTest {
+    @Test
+    public void testPart1() {
+        final var data = getInput(2);
+        final var day  = new Day02();
+        
+        assertEquals(day.part1(data), "", "Part 1");
+    }
+    
+    @Test
+    public void testPart2() {
+        final var data = getInput(2);
+        final var day  = new Day02();
+        
+        assertEquals(day.part2(data), "", "Part 2");
+    }
+}
