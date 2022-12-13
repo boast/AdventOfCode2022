dad06bac9989fe1e921eb5a4617c4f4e6f8e1343
Dave Thalmann <thalmann@sogedes.com>
2022-12-13 16:04:53 +0100

    Optimized solution to use gson instead of parsing on our own...

--- src/aoc/days/Day13.java
+++ src/aoc/days/Day13.java
@@ -2,6 +2,8 @@ package aoc.days;
 
 import aoc.common.CollectionUtil;
 import aoc.common.Day;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 import org.jetbrains.annotations.NonNls;
 
 import java.util.*;
@@ -11,12 +13,9 @@ import java.util.regex.Pattern;
  * <b>Day 13: Distress Signal</b><br>
  * I love queues.<br>
  * Now here there are two challenges and if you do part 1 right, part 2 is actually trivial to solve.<br>
- * The first challenge is to parse the input into a very generic structure. I opted for a queue of objects, which inside
- * are really just more queues or actual integers. You absolutely have to disable inspections if you program that
- * generic or your IDE will scream at you.<br>
- * The parser works recursively through the input, which is also split to each character and consumed by the parsing
- * operations as a queue (stream).<br>
- * The second challenge is a classical compare operation (-1, 0, 1) which again works recursively. The compare step is
+ * The first challenge is to parse the input into a very generic structure. After a first implementation using queues, I
+ * got the idea that this is valid JSON and can be parsed into a generic structure using Gson.<br>
+ * The second challenge is a classical compare operation (-1, 0, 1) which works recursively. The compare step is
  * separated according to instructions, first trying to compare integers, then optionally wrapping a single integer in
  * a new queue and then comparing the queues recursively. One thing to keep in mind is not to actually modify the values
  * and always work on clones / new items.<br>
@@ -27,18 +26,17 @@ import java.util.regex.Pattern;
  */
 @NonNls
 public final class Day13 implements Day {
-    
-    private static final Pattern EMPTY = Pattern.compile("");
     private static final Pattern DIGIT = Pattern.compile("\\d");
     
     @Override
     public Object part1(final List<String> input) {
-        var packetSum   = 0;
-        var packetIndex = 1;
+        final var g           = new Gson();
+        var       packetSum   = 0;
+        var       packetIndex = 1;
         
         for (final var packets : CollectionUtil.partition(input, 3)) {
-            final var left  = parsePacket(new ArrayDeque<>(Arrays.asList(EMPTY.split(packets.get(0)))));
-            final var right = parsePacket(new ArrayDeque<>(Arrays.asList(EMPTY.split(packets.get(1)))));
+            final var left  = g.fromJson(packets.get(0), Object.class);
+            final var right = g.fromJson(packets.get(1), Object.class);
             
             if (comparePacket(left, right) == -1) {
                 packetSum += packetIndex;
@@ -52,17 +50,18 @@ public final class Day13 implements Day {
     
     @Override
     public Object part2(final List<String> input) {
-        final var packets = new ArrayList<ArrayDeque<Object>>();
+        final var g       = new Gson();
+        final var packets = new ArrayList<>();
         
         for (final var packet : input) {
             if (packet.isEmpty()) {
                 continue;
             }
-            packets.add(parsePacket(new ArrayDeque<>(Arrays.asList(EMPTY.split(packet)))));
+            packets.add(g.fromJson(packet, Object.class));
         }
         
-        final var dividerPacket1 = parsePacket(new ArrayDeque<>(Arrays.asList(EMPTY.split("[[2]]"))));
-        final var dividerPacket2 = parsePacket(new ArrayDeque<>(Arrays.asList(EMPTY.split("[[6]]"))));
+        final var dividerPacket1 = g.fromJson("[[2]]", Object.class);
+        final var dividerPacket2 = g.fromJson("[[6]]", Object.class);
         
         packets.add(dividerPacket1);
         packets.add(dividerPacket2);
@@ -78,21 +77,21 @@ public final class Day13 implements Day {
         // are not in the right order. Otherwise, the inputs are the same integer; continue checking the next part of
         // the input.
         
-        if (left instanceof Integer && right instanceof Integer) {
-            return Integer.compare((Integer) left, (Integer) right);
+        if (left instanceof Double && right instanceof Double) { // Gson converts all numbers to Double
+            return Double.compare((Double) left, (Double) right);
         }
         
         // If exactly one value is an integer, convert the integer to a list which contains that integer as its only
         // value, then retry the comparison.
         
         //noinspection all
-        final var leftQueue = left instanceof ArrayDeque ? new ArrayDeque<>((ArrayDeque) left) : new ArrayDeque<>() {
+        final var leftQueue = left instanceof ArrayList ? new ArrayDeque<>((ArrayList) left) : new ArrayDeque<>() {
             {
                 add(left);
             }
         };
         //noinspection all
-        final var rightQueue = right instanceof ArrayDeque ? new ArrayDeque<>((ArrayDeque) right) : new ArrayDeque<>() {
+        final var rightQueue = right instanceof ArrayList ? new ArrayDeque<>((ArrayList) right) : new ArrayDeque<>() {
             {
                 add(right);
             }
@@ -117,51 +116,5 @@ public final class Day13 implements Day {
                 return compare;
             }
         }
-    }
-    
-    private static ArrayDeque<Object> parsePacket(final ArrayDeque<String> packetData) {
-        final var result = new ArrayDeque<>();
-        
-        // A packet starts with a '[', with poll() we consume it
-        if (!Objects.equals(packetData.poll(), "[")) {
-            throw new IllegalArgumentException("Expected '['");
-        }
-        
-        var digitBuffer = new StringBuilder(2);
-        
-        while (!Objects.equals(packetData.peek(), "]")) {
-            // We have a sub-package, recurse
-            if (Objects.equals(packetData.peek(), "[")) {
-                result.add(parsePacket(packetData));
-                // More follow, remove the comma
-                if (Objects.equals(packetData.peek(), ",")) {
-                    packetData.poll();
-                }
-                
-                continue;
-            }
-            
-            // Now the token is either a digit or a comma, the digits needs to be buffered and then added
-            final var token = packetData.poll();
-            assert token != null;
-            
-            if (DIGIT.matcher(token).matches()) {
-                digitBuffer.append(token);
-            } else if (token.equals(",")) {
-                result.add(Integer.parseInt(digitBuffer.toString()));
-                digitBuffer = new StringBuilder(2);
-            }
-        }
-        // Check the digit buffer for the last digit
-        if (!digitBuffer.isEmpty()) {
-            result.add(Integer.parseInt(digitBuffer.toString()));
-        }
-        
-        // A packet ends with a ']', with poll() we consume it
-        if (!Objects.equals(packetData.poll(), "]")) {
-            throw new IllegalArgumentException("Expected ']'");
-        }
-        
-        return result;
     }
 }
